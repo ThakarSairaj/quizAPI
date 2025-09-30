8a1780b55fbeb7e41180b1b4739f6fbbbab110c3
ThakarSairaj <178469889+ThakarSairaj@users.noreply.github.com>
2025-09-30 15:31:39 +0530

    implement route to add the question

--- src/routes/quiz.routes.js
+++ src/routes/quiz.routes.js
@@ -4,5 +4,6 @@ const quizController = require('../controller/quiz.controller');
 const router = express.Router();
 
 router.post('/create', quizController.createQuiz);
+router.post('/:id/questions', quizController.addQuestion);
 
 module.exports = router;
