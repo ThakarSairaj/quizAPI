eca0997589b979868c40d228df18ce1eca61a997
ThakarSairaj <178469889+ThakarSairaj@users.noreply.github.com>
2025-09-30 12:09:34 +0530

    start express server configure the port

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello")
+
+const app = require('./src/app');
+
+const port = process.env.port || 2800;
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
