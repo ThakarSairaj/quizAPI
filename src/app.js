63ec1fd00477792cf07456c29b8b84814b6206e7
ThakarSairaj <178469889+ThakarSairaj@users.noreply.github.com>
2025-09-30 12:08:01 +0530

    configue express app and routs for application

--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
 const express = require('express');
-// const
+const app = express();
+const quizRoutes = require('./routes/quiz.routes');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/quizzes', quizRoutes);
+
+app.get('/status', (req, res) =>{
+    res.json({
+        message: 'Quiz API is running',
+        version: '1.0.0'
+    });
+});
+
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        success: false,
+        message: 'Internal server error',
+        error: error.message
+    });
+});
+
+module.exports = app;
