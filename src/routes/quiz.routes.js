b2dc24c19e10d38067ba47087dbefb9c57cba82f
ThakarSairaj <178469889+ThakarSairaj@users.noreply.github.com>
2025-10-02 18:28:25 +0530

    add route for submitting answer

--- src/routes/quiz.routes.js
+++ src/routes/quiz.routes.js
@@ -3,8 +3,9 @@ const quizController = require('../controller/quiz.controller');
 
 const router = express.Router();
 
-router.post('/create', quizController.createQuiz);
-router.post('/:id/questions', quizController.addQuestion);
-router.get('/:id/getQuizQuestions', quizController.getQuizQuestion);
+router.post('/quizzes', quizController.createQuiz);
+router.post('/quizzes/:id/questions', quizController.addQuestion);
+router.get('/quizzes/:id', quizController.getQuizQuestion);
+router.post('/quizzes/:id/submit', quizController.submitAnswers);
 
 module.exports = router;
