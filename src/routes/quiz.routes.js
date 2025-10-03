8a82f1196b685abd031c0c3da0b06bc99d222e58
ThakarSairaj <178469889+ThakarSairaj@users.noreply.github.com>
2025-10-03 20:39:50 +0530

    rename routes for easier access and added char validation

--- src/routes/quiz.routes.js
+++ src/routes/quiz.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const quizController = require('../controller/quiz.controller');
+const { validateQuestionMiddleware } = require('../valid/validate');
+
 
 const router = express.Router();
 
 router.post('/quizzes', quizController.createQuiz);
-router.post('/quizzes/:id/questions', quizController.addQuestion);
+router.post('/quizzes/:id/questions', validateQuestionMiddleware, quizController.addQuestion);
 router.get('/quizzes/:id', quizController.getQuizQuestion);
 router.post('/quizzes/:id/submit', quizController.submitAnswers);
 
